7f4e1db5a04cdb41b93ffe49e56cec9a17af9ae2
Kev <test@example.com>
2016-09-21 11:25:58 +0200

    init

---
+++ Cyclomatic/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cyclomatic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StartRace race = new StartRace();
+            Console.WriteLine(race.Resultado(1,2));
+        }
+    }
+}
---
+++ Cyclomatic/StartRace.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cyclomatic
+{
+    class StartRace
+    {
+
+        public int Resultado(int? primerNumero, int segundoNumero)
+        {
+            try
+            {
+                if(primerNumero.Value == 1)
+                    return primerNumero.Value + segundoNumero;
+                if (primerNumero.Value == 12)
+                    return primerNumero.Value + segundoNumero;
+                if (primerNumero.Value == 2)
+                    return primerNumero.Value + segundoNumero;
+                if (primerNumero.Value == 3)
+                    return primerNumero.Value + segundoNumero;
+                if (primerNumero.Value == 4)
+                    return primerNumero.Value + segundoNumero;
+                if (primerNumero.Value == 5)
+                    return primerNumero.Value + segundoNumero;
+                return 0;
+            }
+            catch(Exception e)
+            {
+                return e.GetHashCode();
+            }
+        }
+    }
+}
